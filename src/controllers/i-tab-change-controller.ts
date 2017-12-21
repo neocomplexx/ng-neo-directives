1d3e55474ad5836a431153ab79c864e65ae5a9b5
Sebastian Larrieu <slarriue@neocomplexx.com>
2017-12-21 09:41:30 -0300

    New directive

---
+++ src/controllers/i-tab-change-controller.ts
@@ -0,0 +1,6 @@
+
+// TODO: Mover a otra carpeta, no se a donde ponerla
+export interface ITabChangeController {
+
+    canChangeTab(): Promise<boolean>;
+}
--- src/ngb-tabset.directive.ts
+++ src/ngb-tabset.directive.ts
@@ -3,26 +3,35 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs/Subscription';
 import { NeoModalService } from 'ng-neo-modal';
+import { ITabChangeController } from './controllers/i-tab-change-controller';
 
+/**
+ * @author jcangelosi
+ */
 @Directive({
-    selector: '[tabSelectedId]',
+    selector: '[tabSelectedIdNueva], [tabChangeController]',
 })
-export class NgbTabSetDirective implements OnInit, OnDestroy {
+export class NgbTabSetNuevaDirective implements OnInit, OnDestroy {
     protected el: NgbTabset;
     protected tabSelectedIdSubscribe: Subscription;
 
-    @Input() tabSelectedId: BehaviorSubject<string>;
+    @Input() tabSelectedIdNueva: BehaviorSubject<string>;
+
+    @Input() tabChangeController: ITabChangeController;
+
+    protected onTabChanging: boolean;
 
     constructor(protected _el: NgbTabset, protected neoModalService: NeoModalService) {
         this.el = this._el;
+        this.onTabChanging = false;
     }
 
     ngOnInit() {
-        if (this.tabSelectedId) {
-            this.tabSelectedIdSubscribe = this.tabSelectedId.subscribe((tabSelectedId) => {
+        if (this.tabSelectedIdNueva) {
+            this.tabSelectedIdSubscribe = this.tabSelectedIdNueva.subscribe((tabSelectedId) => {
                 if (tabSelectedId.length > 0) {
                     const tabs = this.el as NgbTabset;
-                    const previousValue = this.tabSelectedId.value;
+                    const previousValue = this.tabSelectedIdNueva.value;
                     if (tabs.activeId !== tabSelectedId) {
                         tabs.select(tabSelectedId);
                     }
@@ -37,9 +46,26 @@ export class NgbTabSetDirective implements OnInit, OnDestroy {
 
 
     @HostListener('tabChange', ['$event']) async onTabChange($event: any) {
+        if (this.onTabChanging) {
+            return;
+        }
+        this.onTabChanging = true;
+        if (this.tabChangeController) {
+            $event.preventDefault();
+            if (await this.tabChangeController.canChangeTab()) {
+                this.cambiarTab($event, true);
+            }
+        } else {
+            this.cambiarTab($event);
+        }
+        this.onTabChanging = false;
+    }
+
+    private cambiarTab($event: any, force: boolean = false) {
         const tabSelecting = $event.nextId;
-        if (this.tabSelectedId.value !== tabSelecting) {
-            this.tabSelectedId.next(tabSelecting)
+        if (force || this.tabSelectedIdNueva.value !== tabSelecting) {
+            this.tabSelectedIdNueva.next(tabSelecting);
         }
     }
+
 }
